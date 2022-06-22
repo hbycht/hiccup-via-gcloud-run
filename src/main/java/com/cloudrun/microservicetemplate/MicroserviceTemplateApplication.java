fb993db172f2436ae8ffb7a13495ea68ab58a59d
hbrode <henning.brode@smail.th-koeln.de>
2022-06-22 23:20:14 +0200

    Updated project structure.

--- src/main/java/com/cloudrun/microservicetemplate/MicroserviceTemplateApplication.java
+++ src/main/java/com/cloudrun/microservicetemplate/MicroserviceTemplateApplication.java
@@ -17,16 +17,12 @@
 package com.cloudrun.microservicetemplate;
 
 import javax.annotation.PreDestroy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /** Microservice template for Cloud Run. */
 @SpringBootApplication
 public class MicroserviceTemplateApplication {
-  private static final Logger logger =
-      LoggerFactory.getLogger(MicroserviceTemplateApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(MicroserviceTemplateApplication.class, args);
@@ -35,8 +31,5 @@ public class MicroserviceTemplateApplication {
   /** Register shutdown hook to listen for termination signal. */
   @PreDestroy
   public void tearDown() {
-    // Clean up resources on shutdown
-    logger.info(MicroserviceTemplateApplication.class.getSimpleName() + ": received SIGTERM.");
-    // Flush async logs if needed - current Logback config does not buffer logs
   }
 }
